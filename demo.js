30f886e292e1c53d375673726a0de6e48d10e18a
fuckdcma <144027773+fuckdcma@users.noreply.github.com>
2026-09-15 00:28:47 +0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,17 +1,5 @@
-[{
-	"imdb"="tt12254",
-	"title"="Moana (2026)"
-	"source"="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-	"image"="https://yts.gg/assets/images/movies/moana_2026/medium-cover.jpg",
-	"type"="Phim lẻ",
-	"country"="Âu Mỹ",
-	"year"="2026",
-	"category"="Hoạt Hình"
-	"Quality"="Full HD"
-}]
-
 [
-  {
+	{
     "title": "Phim Chiếu Rạp Mới 2025",
     "stream_url": "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
     "image": "https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=800&q=80",
@@ -22,5 +10,18 @@
     "quality": "4K UltraHD",
     "rating": "9.2",
     "description": "Bản phát hành chuẩn HLS âm thanh vòm dành cho máy chiếu gia đình."
-  }
+	},
+	{
+	"imdb"="tt12254",
+	"title"="Moana (2026)"
+	"stream_url"="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+	"image"="https://yts.gg/assets/images/movies/moana_2026/medium-cover.jpg",
+	"type"="Phim lẻ",
+	"country"="Âu Mỹ",
+	"year"="2026",
+	"category"="Hoạt Hình",
+	"Quality"="Full HD",
+    "rating": "9.2",
+    "description": "Test ting."
+	}
 ]
