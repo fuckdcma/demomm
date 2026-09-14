66d1d57738746ac4b8e4a88826b90a9925cad377
fuckdcma <144027773+fuckdcma@users.noreply.github.com>
2026-09-15 00:19:43 +0700

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,11 @@
+[{
+	"imdb"="tt12254",
+	"title"="Moana (2026)"
+	"source"="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+	"image"="https://yts.gg/assets/images/movies/moana_2026/medium-cover.jpg",
+	"type"="Phim lẻ",
+	"country"="Âu Mỹ",
+	"year"="2026",
+	"category"="Hoạt Hình"
+	"Quality"="Full HD"
+}]
